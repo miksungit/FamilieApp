b67de4b23f28d5ac20eaa73eeb51fbec1991cc5c
Mikael Sundby <500034@student.hin.no>
2017-03-13 18:46:36 +0100

    onClickListener for Groceries

--- app/src/main/java/data/sundby/familieapp/MainActivity.java
+++ app/src/main/java/data/sundby/familieapp/MainActivity.java
@@ -1,16 +1,37 @@
 package data.sundby.familieapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+    // Create an anonymous implementation of OnClickListener
+    private View.OnClickListener groceriesListener = new View.OnClickListener() {
+        public void onClick(View v) {
+
+            Context context = getApplicationContext();
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast groceriesToast = Toast.makeText(context, context.getString(R.string.groceries_toast), duration);
+            groceriesToast.show();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        // Capture our button from layout
+        TextView groceries = (TextView) findViewById(R.id.tv_groceries);
+        // Register the onClick listener with the implementation above
+        groceries.setOnClickListener(groceriesListener);
 
     }
 }
